248664e5275aaf2f1684a3678a12d52e04cde8ae
Vivek Modi <vivek.modi@abzooba.com>
2019-08-08 13:00:26 +0530

    Utilities added for resizing and straightening
    Signed-off-by: Vivek Modi <vivek.modi@abzooba.com>

--- deep_learning/OCR/ocr_testing.py
+++ deep_learning/OCR/ocr_testing.py
@@ -12,20 +12,21 @@ else:
 
 langs = tools[0].get_available_languages()
 print ("Available languages", langs)
+images = 'test2.jpg'
 
-text = tools[0].image_to_string(Image.open('first_page.png'), lang=langs[0], builder=pyocr.builders.TextBuilder())
+text = tools[0].image_to_string(Image.open(images), lang=langs[0], builder=pyocr.builders.TextBuilder())
 
-word_box = tools[0].image_to_string(Image.open('first_page.png'), lang=langs[0], builder=pyocr.builders.WordBoxBuilder())
+word_box = tools[0].image_to_string(Image.open(images), lang=langs[0], builder=pyocr.builders.WordBoxBuilder())
 
-lines_word_boxes = tools[0].image_to_string(Image.open('first_page.png'), lang=langs[0], builder=pyocr.builders.LineBoxBuilder())
+lines_word_boxes = tools[0].image_to_string(Image.open(images), lang=langs[0], builder=pyocr.builders.LineBoxBuilder())
 
-digits = tools[0].image_to_string(Image.open('first_page.png'), lang=langs[0], builder=pyocr.builders.DigitBuilder())
+digits = tools[0].image_to_string(Image.open(images), lang=langs[0], builder=pyocr.builders.DigitBuilder())
 
-print(lines_word_boxes)
+print(text)
 
 if tools[0].can_detect_orientation():
     try:
-        orientation = tools[0].detect_orientation(Image.open('first_page.png'), lang=langs[0])
+        orientation = tools[0].detect_orientation(Image.open(images), lang=langs[0])
     except pyocr.PyocrException as exc:
         print(exc)
     print("Orientation: {}".format(orientation))
--- deep_learning/OCR/utilities.py
+++ deep_learning/OCR/utilities.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class ImageProcessing:
@@ -8,7 +9,7 @@ class ImageProcessing:
         self.image = image
 
     def resize_image(self):
-        self.image = cv2.resize(self.image, (600, 800), interpolation=cv2.INTER_AREA)
+        self.image = cv2.resize(self.image, (600, 700), interpolation=cv2.INTER_AREA)
 
     def thresholding(self, g_image, inp):
         new_image = g_image
@@ -16,9 +17,9 @@ class ImageProcessing:
         thresh1 = cv2.adaptiveThreshold(new_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 2)
         thresh2 = cv2.threshold(new_image, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)[1]
         images = [new_image, thresh1, thresh2]
-        for img in images:
-            cv2.imshow("images", img)
-            cv2.waitKey(0)
+        # for img in images:
+        #     cv2.imshow("images", img)
+        #     cv2.waitKey(0)
         if inp == 1:
             return thresh2
 
@@ -42,13 +43,29 @@ class ImageProcessing:
         matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
         rotated = cv2.warpAffine(self.image, matrix, (width, height), flags=cv2.INTER_CUBIC,
                                  borderMode=cv2.BORDER_REPLICATE)
-        cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle), (10, 30), cv2.FONT_HERSHEY_COMPLEX,
-                    0.7, (0, 0, 255), 2)
+        self.image = rotated
+        # cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle), (10, 30), cv2.FONT_HERSHEY_COMPLEX,
+        #             0.7, (0, 0, 255), 2)
         cv2.imshow("Rotated", rotated)
         cv2.waitKey(0)
 
+    def draw_roi(self):
+        # gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        # gray_image = self.image
+        gray_image = cv2.bitwise_not(self.image)
+        thresh = self.thresholding(gray_image, 1)
+        edges = cv2.Canny(thresh, 200, 210)
+        contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        self.image = cv2.drawContours(gray_image, contours, -1, (255, 255, 0), 1)
+        cv2.imshow("contours", self.image)
+        cv2.waitKey(0)
+        # plt.imshow(edges)
+        # plt.show()
+
 
 if __name__ == '__main__':
-    Image = cv2.imread('test2.jpg', 0)
+    image_list = ['first_page.png', 'test2.jpg']
+    Image = cv2.imread(image_list[1], 0)
     processing = ImageProcessing(Image)
     processing.straighten_image()
+    processing.draw_roi()
